ff243109ddcdf28de868e1e5f31ec85c635b912e
MichoFrost <Michofrost@gmail.com>
2023-05-07 14:21:14 +0200

    animations done!

--- script.js
+++ script.js
@@ -4,7 +4,35 @@ const closeIcon = document.querySelector(".close_icon");
 const imageGrid = document.querySelector(".image_grid_container");
 const mainImage = document.querySelector(".main_image");
 
-console.log(closeIcon);
+const dragon = document.querySelector(".dragon");
+const dice = document.querySelector(".dice");
+const penPaper = document.querySelector(".pen_paper");
+const sword = document.querySelector(".sword");
+
+const text = document.querySelector(".text");
+
+window.addEventListener("load", startAnimations);
+
+function startAnimations() {
+  console.log("activateSVGs");
+  console.log(dragon);
+
+  setTimeout(function () {
+    dragon.classList.add("pop-up");
+  }, 1000);
+  setTimeout(function () {
+    sword.classList.add("pop-up");
+  }, 1200);
+  setTimeout(function () {
+    dice.classList.add("pop-up");
+  }, 1400);
+  setTimeout(function () {
+    penPaper.classList.add("pop-up");
+  }, 1600);
+  setTimeout(function () {
+    text.classList.add("visible");
+  }, 2000);
+}
 
 imageGrid.addEventListener("click", chooseImage);
 closeIcon.addEventListener("click", closeModal);
