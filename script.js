f184809b424ea627b9a74db2e132acebd86025f9
MichoFrost <Michofrost@gmail.com>
2023-05-07 12:20:44 +0200

    Modal done

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-// let landingPage = document.querySelector(".landing_page");
+const overlay = document.querySelector(".overlay");
+const modal = document.querySelector(".modal");
+const closeIcon = document.querySelector(".close_icon");
+const imageGrid = document.querySelector(".image_grid_container");
+const mainImage = document.querySelector(".main_image");
 
-// console.log(landingPage);
+console.log(closeIcon);
 
-// landingPage.style.background = "pink";
+imageGrid.addEventListener("click", chooseImage);
+closeIcon.addEventListener("click", closeModal);
+
+// OPEN MODAL
+
+function openModal() {
+  console.log("openModal");
+  overlay.classList.add("visible");
+  modal.classList.add("visible");
+}
+
+// CLOSE MODAL
+
+function closeModal() {
+  console.log("closeModal");
+  overlay.classList.remove("visible");
+  modal.classList.remove("visible");
+}
+
+function chooseImage(e) {
+  // console.log("chooseImage");
+  if (!e.target.classList.contains("image")) return;
+  console.log(e.target);
+
+  mainImage.src = e.target.src;
+  openModal();
+}
